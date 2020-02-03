3453ea9aa413363e69a00d492d95bd82f74e5f9e
Eetu Rinta-Jaskari <ermurj@hotmail.com>
2020-02-03 21:50:28 +0200

    Minor changes to Vagrant configuration

--- vagrant/installation.sh
+++ vagrant/installation.sh
@@ -12,9 +12,9 @@ sudo apt-get update
 
 ## Install psql and config it
 sudo apt-get -y -q install postgresql
+sudo sh -c 'echo "local   weto5_master    weto                                    md5" >> /etc/postgresql/9.3/main/pg_hba.conf'
+sudo sh -c 'echo "local   weto5_kurssi    weto                                    md5" >> /etc/postgresql/9.3/main/pg_hba.conf'
 sudo -u postgres psql -f /vagrant/init.sql
-sudo sh -c 'echo "local    weto5_master    weto    md5" >> /etc/postgresql/9.3/main/pg_hba.conf'
-sudo sh -c 'echo "local    weto5_kurssi    weto    md5" >> /etc/postgresql/9.3/main/pg_hba.conf'
 sudo service postgresql restart
 
 ## Initialize the database
@@ -29,10 +29,12 @@ sudo apt-get -y -q install openjdk-8-jdk
 
 ## Install tomcat
 sudo useradd -r -m -U -d /opt/tomcat -s /bin/false tomcat
+sudo usermod -a -G tomcat vagrant
 wget -nv http://www.nic.funet.fi/pub/mirrors/apache.org/tomcat/tomcat-9/v9.0.30/bin/apache-tomcat-9.0.30.tar.gz -P /tmp
 sudo tar -xf /tmp/apache-tomcat-9.*.tar.gz -C /opt/tomcat
 sudo ln -s /opt/tomcat/apache-tomcat-9.0.30 /opt/tomcat/latest
 sudo chown -RH tomcat: /opt/tomcat/latest
+sudo chmod -R 775 /opt/tomcat/latest
 sudo sh -c 'chmod +x /opt/tomcat/latest/bin/*.sh'
 
 # Set tomcat up for systemctl
