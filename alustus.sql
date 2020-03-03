b9c9067c31fed93cf6ad1ccb33c822af710c4acb
Eetu Rinta-Jaskari <ermurj@hotmail.com>
2020-03-03 10:41:14 +0200

    Fixed the default user

--- alustus.sql
+++ alustus.sql
@@ -1,4 +1,4 @@
-insert into useraccount (loginname, password, firstname, lastname, email) values ('ope', '1000:514aa8d861d94c29fcf3644c955f4668bb83ce512017a06f:08703590a4420c70747af05a5d9b7fb40c9611cf6f609034', 'Ope', 'Ope', 'ope@o.pe');
+insert into useraccount (loginname, password, firstname, lastname, email) values ('ope@tuni.fi', '1000:514aa8d861d94c29fcf3644c955f4668bb83ce512017a06f:08703590a4420c70747af05a5d9b7fb40c9611cf6f609034', 'Ope', 'Ope', 'ope@o.pe');
 insert into databasepool (name, url, username, password) values ('kurssi', 'jdbc:postgresql://localhost/weto5_kurssi', 'weto', 'weto');
 insert into admin select last_value from useraccount_id_seq;
 insert into subject (name) values ('Computer Science');
