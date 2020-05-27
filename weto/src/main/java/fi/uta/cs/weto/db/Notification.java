90f73d48fb99064725b31e5cdbf3aa5bc4e7c522
Juha-Pekka Salonen <juha-pekka.salonen@tuni.fi>
2020-05-27 18:00:09 +0300

    Added permission expiration to message resources

--- weto/src/main/java/fi/uta/cs/weto/db/Notification.java
+++ weto/src/main/java/fi/uta/cs/weto/db/Notification.java
@@ -15,9 +15,10 @@ import java.util.*;
 public class Notification extends SqlAssignableObject implements Cloneable {
     public static final String FORUM_POST = "forum_post";
     public static final String MASS_NOTIFICATION = "mass_notification";
+    public static final String PERMISSION_EXPIRATION = "permission_expiration";
 
     // Create a list of the different types
-    public static final List<String> notificationTypes = Collections.unmodifiableList(Arrays.asList(FORUM_POST, MASS_NOTIFICATION));
+    public static final List<String> notificationTypes = Collections.unmodifiableList(Arrays.asList(FORUM_POST, MASS_NOTIFICATION, PERMISSION_EXPIRATION));
 
     private static final Logger logger = Logger.getLogger(Notification.class);
 
