b97e97a2e40c7b930dc9cd060b3e542aee85ab51
Juha-Pekka Salonen <juha-pekka.salonen@tuni.fi>
2020-05-28 18:43:34 +0300

    Added automatic teachers notifications when permission expires

--- weto/src/main/java/fi/uta/cs/weto/util/NotificationManager.java
+++ weto/src/main/java/fi/uta/cs/weto/util/NotificationManager.java
@@ -1,33 +1,34 @@
+package fi.uta.cs.weto.util;
 
-        package fi.uta.cs.weto.util;
+import com.opensymphony.xwork2.ActionContext;
+import fi.uta.cs.weto.db.*;
+import fi.uta.cs.weto.model.ClusterType;
+import fi.uta.cs.weto.model.PermissionType;
+import fi.uta.cs.weto.model.Tab;
+import fi.uta.cs.weto.model.WetoTimeStamp;
+import org.apache.log4j.Logger;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
-        import com.opensymphony.xwork2.ActionContext;
-        import fi.uta.cs.weto.db.*;
-        import fi.uta.cs.weto.model.WetoTimeStamp;
-        import org.apache.log4j.Logger;
-        import org.apache.velocity.Template;
-        import org.apache.velocity.VelocityContext;
-        import org.apache.velocity.app.VelocityEngine;
-        import org.apache.velocity.runtime.RuntimeConstants;
-        import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+import javax.mail.MessagingException;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-        import javax.mail.MessagingException;
-        import javax.servlet.ServletContextEvent;
-        import javax.servlet.ServletContextListener;
-        import javax.servlet.annotation.WebListener;
-        import java.io.*;
-        import java.sql.Connection;
-        import java.sql.SQLException;
-        import java.util.ArrayList;
-        import java.util.GregorianCalendar;
-        import java.util.HashMap;
-        import java.util.HashSet;
-        import java.util.concurrent.Executors;
-        import java.util.concurrent.ScheduledExecutorService;
-        import java.util.concurrent.TimeUnit;
-
-
-        @WebListener()
+@WebListener()
 public class NotificationManager implements ServletContextListener {
     private static final Logger logger = Logger.getLogger(NotificationManager.class);
 
@@ -43,9 +44,11 @@ public class NotificationManager implements ServletContextListener {
 
         int notificationEmailInterval = Integer.parseInt(
                 WetoUtilities.getPackageResource("notification.emailInterval.minutes"));
+        int permissionExpirationCheckInterval = Integer.parseInt(
+               WetoUtilities.getPackageResource("notification.permissionExpirationCheckInterval.minutes"));
         scheduler.scheduleAtFixedRate(new NotificationEmailTask(), 1, notificationEmailInterval, TimeUnit.MINUTES);
 
-        scheduler.scheduleAtFixedRate(new DeadlineNotificationTask(), 1, notificationEmailInterval, TimeUnit.MINUTES);
+        scheduler.scheduleAtFixedRate(new DeadlineNotificationTask(), 1, permissionExpirationCheckInterval, TimeUnit.MINUTES);
     }
 
     @Override
@@ -58,6 +61,12 @@ public class NotificationManager implements ServletContextListener {
 
         @Override
         public void run() {
+
+            ArrayList<Integer> triggeringPermissions = new ArrayList<>();
+            triggeringPermissions.add(PermissionType.SUBMISSION.getValue());
+            triggeringPermissions.add(PermissionType.GRADING.getValue());
+            triggeringPermissions.add(PermissionType.RESULTS.getValue());
+            triggeringPermissions.add(PermissionType.GRADE_CHALLENGE.getValue());
 
             Connection masterCon = connectionManager.getConnection("master");
             ArrayList<Permission> activeTasks = new ArrayList<>();
@@ -73,7 +82,7 @@ public class NotificationManager implements ServletContextListener {
             for (Permission masterTask : activeTasks) {
                 try {
 
-                    int databaseID = CourseImplementation.select1ByMasterTaskId(masterCon,masterTask.getId()).getDatabaseId();
+                    int databaseID = CourseImplementation.select1ByMasterTaskId(masterCon,masterTask.getTaskId()).getDatabaseId();
 
                     //iterate a course database only once
                     if (!databases.contains(databaseID)) {
@@ -90,6 +99,7 @@ public class NotificationManager implements ServletContextListener {
                          for (Permission permission : allActiveCoursePermissions) {
 
                              if (permission.getEndDate() == null) {
+                                 continue;
                                  }
                              try {
                                  WetoTimeStamp permissionStamp = new WetoTimeStamp(permission.getEndDate());
@@ -104,6 +114,39 @@ public class NotificationManager implements ServletContextListener {
                              }
 
                              //Implement teacher notifications here
+                             if (triggeringPermissions.contains(permission.getType())) {
+                                 int taskId = permission.getTaskId();
+                                 Task task = Task.select1ById(courseCon, taskId);
+                                 int rootTaskId = task.getRootTaskId();
+                                 try {
+                                     int masterCourseId = CourseImplementation.select1ByDatabaseIdAndCourseTaskId(masterCon, databaseID, rootTaskId).getMasterTaskId();
+
+                                     // Create notification's message for teachers.
+                                     HashMap<String, String> templateValueMap = new HashMap<>();
+                                     templateValueMap.put("&permission;", WetoUtilities.getMessageResource(PermissionType.getType(permission.getType()).getProperty()));
+                                     templateValueMap.put("&task;", task.getName());
+                                     templateValueMap.put("&expirationTime;", new WetoTimeStamp(permission.getEndDate()).toString());
+
+                                     String notificationMessage = Notification.getMessageFromTemplate(Notification.PERMISSION_EXPIRATION, templateValueMap);
+
+                                     // Create link.
+                                     String link = WetoUtilities.getPackageResource("weto.appBaseUrl")
+                                         + "/viewPermissions.action?taskId=" + taskId
+                                         + "&tabId=" + Tab.PERMISSIONS.getValue()
+                                         + "&dbId=" + databaseID;
+
+
+                                     for (UserTaskView teacher : UserTaskView.selectByTaskIdAndClusterType(courseCon, rootTaskId, ClusterType.TEACHERS.getValue())) {
+                                         Notification notification = new Notification(teacher.getUserId(), masterCourseId, Notification.PERMISSION_EXPIRATION, link);
+                                         notification.setMessage(notificationMessage);
+                                         notification.createNotification(masterCon, courseCon);
+
+                                     }
+                                 }
+                                 catch (Exception ignored) {
+                                 }
+                             }
+
                             //Check if permission is submission permission
                             if (permission.getType() == 1) {
                                 HashSet<Integer> notSubmittedStudents = new HashSet<>();
@@ -117,17 +160,19 @@ public class NotificationManager implements ServletContextListener {
                                 boolean isAllUsersPermission = permission.getUserRefId() == null;
                                 if (isAllUsersPermission) {
                                     try {
-                                        Task temp = Task.select1ById(courseCon, permission.getTaskId());
-                                        ArrayList<ClusterMember> memberList = ClusterMember.selectByClusterId(courseCon, temp.getRootTaskId());
-
-                                        for (ClusterMember member : memberList) {
-                                            notSubmittedStudents.add(member.getUserId());
+                                        //Task temp = Task.select1ById(courseCon, permission.getTaskId());
+                                        //ArrayList<ClusterMember> memberList = ClusterMember.selectByClusterId(courseCon, temp.getRootTaskId());
+                                        for (UserTaskView student : UserTaskView.selectByTaskIdAndClusterType(courseCon, permissionsTask.getId(), ClusterType.STUDENTS.getValue())) {
+                                            notSubmittedStudents.add(student.getUserId());
                                         }
+
+                                        //for (ClusterMember member : memberList) {
+                                            //notSubmittedStudents.add(member.getUserId());
+                                        //}
                                     } catch (Exception e) {
                                         logger.error(e);
                                     }
                                 }
-
 
                                 //one tasks submissions
                                 ArrayList<Submission> taskSubmissions = Submission.selectByTaskId(courseCon, assignmentTaskID);
@@ -149,24 +194,42 @@ public class NotificationManager implements ServletContextListener {
                                     for (int student : notSubmittedStudents) {
                                         UserAccount user = UserAccount.select1ById(courseCon, student);
                                         UserAccount masterUser = UserAccount.select1ByLoginName(masterCon, user.getLoginName());
-                                        int taskID = permission.getTaskId();
+                                        //int taskID = permission.getTaskId();
 
-                                        int courseID = permissionsTask.getRootTaskId();
+                                        int masterCourseID = CourseImplementation.select1ByDatabaseIdAndCourseTaskId(masterCon, databaseID, permissionsTask.getRootTaskId()).getMasterTaskId();
 
-                                        StringBuilder link = new StringBuilder();
-                                        link.append("/weto5/viewSubmissions.action?taskId=").append(taskID).append("&tabId=4&dbId=").append(databaseID);
+                                        // Create notification's message for students who has not made submission.
+                                        HashMap<String, String> valueMap = new HashMap<>();
+                                        valueMap.put("&task;", permissionsTask.getName());
+                                        valueMap.put("&expirationTime;", new WetoTimeStamp(permission.getEndDate()).toString());
+
+                                        String message = Notification.getMessageFromTemplate(Notification.SUBMISSION_DEADLINE, valueMap);
+
+                                        // Create link.
+                                        String notificationLink = WetoUtilities.getPackageResource("weto.appBaseUrl")
+                                                + "/viewSubmissions.action?taskId=" + permissionsTask.getId()
+                                                + "&tabId=" + Tab.SUBMISSIONS.getValue()
+                                                + "&dbId=" + databaseID;
+
+                                        Notification notification = new Notification(student, masterCourseID, Notification.SUBMISSION_DEADLINE, notificationLink);
+                                        notification.setMessage(message);
+                                        notification.createNotification(masterCon, courseCon);
+
+
+                                        //StringBuilder link = new StringBuilder();
+                                        //link.append("/weto5/viewSubmissions.action?taskId=").append(taskID).append("&tabId=4&dbId=").append(databaseID);
 
                                         ///ActionContext.getContext().getName()
                                         ////weto5/viewForumTopic.action?taskId=8&tabId=5&dbId=1&topicId=43
                                         //get courses ID in the master database
-                                        int masterTaskID = CourseImplementation.select1ByDatabaseIdAndCourseTaskId(masterCon,databaseID,courseID).getMasterTaskId();
-                                        try {
-                                            Notification notification = new Notification(masterUser.getId(), masterTaskID, Notification.DEADLINE, link.toString());
-                                            notification.setMessage(permissionsTask.getName());
-                                            notification.createNotification(masterCon, courseCon);
-                                        } catch (Exception e) {
-                                            logger.error(e);
-                                        }
+                                        //int masterTaskID = CourseImplementation.select1ByDatabaseIdAndCourseTaskId(masterCon,databaseID,courseID).getMasterTaskId();
+                                        //try {
+                                            //Notification notification = new Notification(masterUser.getId(), masterTaskID, Notification.SUBMISSION_DEADLINE, link.toString());
+                                            //notification.setMessage(permissionsTask.getName());
+                                            //notification.createNotification(masterCon, courseCon);
+                                        //} catch (Exception e) {
+                                            //logger.error(e);
+                                        //}
 
 
                                     }
