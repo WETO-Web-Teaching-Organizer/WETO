9706d7e0d6d429943d3a44f66f25a9e981252777
Miika Johansson <miika.johansson@outlook.com>
2020-10-25 22:50:36 +0200

    Add APIs for creating, completing, and deleting submissions

--- weto-front/src/backend-api.js
+++ weto-front/src/backend-api.js
@@ -26,6 +26,31 @@ export default {
     return AXIOS.post('/viewJSONCourseTask', {dbId: db, taskId: task, tabId: tab})
   },
   // Submission APIs
+  createSubmission(submitterId, dbId, taskId, tabId) {
+    let formData = new FormData();
+    formData.set("submitterId", submitterId)
+    formData.set("dbId", dbId)
+    formData.set("taskId", taskId)
+    formData.set("tabId", tabId)
+    return AXIOS.post("/createSubmission", formData)
+  },
+  completeSubmission(submissionId, dbId, taskId, tabId) {
+    let formData = new FormData();
+    formData.set("submissionId", submissionId)
+    formData.set("dbId", dbId)
+    formData.set("taskId", taskId)
+    formData.set("tabId", tabId)
+    return AXIOS.post("/completeSubmission", formData)
+  },
+  deleteSubmission(submissionId, submitted, dbId, taskId, tabId) {
+    let formData = new FormData();
+    formData.set("submissionId", submissionId)
+    formData.set("submitted", submitted)
+    formData.set("dbId", dbId)
+    formData.set("taskId", taskId)
+    formData.set("tabId", tabId)
+    return AXIOS.post("/ajaxDeleteSubmissionConfirm", formData)
+  },
   saveQuizAnswer(formData) {
     return AXIOS.post("/saveQuizAnswer", formData)
   },
@@ -38,6 +63,10 @@ export default {
   // Create a document entry for the submission
   addSubmissionFile(fileName, submissionId, dbId, taskId, tabId) {
     return AXIOS.post("/addSubmissionFile", {documentFileFileName: fileName, submissionId, dbId, taskId, tabId})
+  },
+  // TODO: returns the file contents in response, but doesn't initialize file download
+  downloadSubmissionFile(documentId, dbId, taskId, tabId) {
+    return AXIOS.get("/downloadDocument", { params: {documentId, dbId, taskId, tabId} })
   },
   // Deletes the actual file and the document entry from the submission
   deleteSubmissionFile(documentId, dbId, taskId, tabId) {
