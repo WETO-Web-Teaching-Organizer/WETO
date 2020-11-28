edfeba6c6ee209edbc2fcf65b70da908429102bc
Paavo Keski-Orvola <paavo.keski-orvola@tuni.fi>
2020-11-28 17:03:25 +0200

    Add changes TaskGrade

--- weto-front/src/components/TaskGrade.vue
+++ weto-front/src/components/TaskGrade.vue
@@ -1,72 +1,136 @@
 <template>
     <div>
-        <v-simple-table v-if="autoGrading">
-            <template v-slot:default>
-                <thead>
-                    <tr>
-                        <th class="text-left">
-                            Test #
-                        </th>
-                        <th class="text-left">
-                            Score
-                        </th>
-                        <th class="text-left">
-                            Time
-                        </th>
-                        <th class="text-left">
-                            Feedback
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr
-                        v-for="item in grades"
-                        :key="item.testNo"
+        <div v-if="autoGrading">
+            <v-simple-table>
+                <template v-slot:default>
+                    <thead>
+                        <tr>
+                            <th class="text-left">
+                                Test #
+                            </th>
+                            <th class="text-left">
+                                Score
+                            </th>
+                            <th class="text-left">
+                                Time
+                            </th>
+                            <th class="text-left">
+                                Feedback
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr
+                            v-for="item in grades"
+                            :key="item.testNo"
+                        >
+                            <td>{{ item.testNo }}</td>
+                            <td>
+                                <v-chip
+                                    :color="autoGradeColor(item.testScore)"
+                                >
+                                    {{ item.testScore }}
+                                </v-chip>
+                            </td>
+                            <td>{{ item.processingTime }} </td>
+                            <td>
+                                <v-chip
+                                    :color="autoGradeColor(item.testScore)"
+                                >
+                                    {{ item.feedback }}
+                                </v-chip>
+                            </td>
+                        </tr>
+                    </tbody>
+                </template>
+            </v-simple-table>
+        </div>
+        <div v-else>
+            <v-data-table
+                :headers="gradeHeaders"
+                :items="grades"
+                :expanded.sync="expanded"
+                item-key="name"
+                show-expand
+                class="table-header"
+                disable-sort
+                hide-default-footer
+            >
+                <template v-slot:top class="table-header">
+                    <v-toolbar flat>
+                        <v-toolbar-title>Grade</v-toolbar-title>
+                        <v-spacer></v-spacer>
+                        <v-chip
+                            :color="lightgrey"
+                        >Rating range: 
+                            <v-avatar
+                                class="grey darken-1"
+                                right>
+                                <strong>{{minimum}}</strong>
+                            </v-avatar>
+                            <v-avatar
+                                right>
+                            <strong>-</strong>
+                            </v-avatar>
+                            <v-avatar
+                                class="grey darken-1"
+                                right>
+                                <strong>{{maximum}}</strong>
+                            </v-avatar>
+                        </v-chip>
+                    </v-toolbar>
+                </template>
+                <template v-slot:item.rating="{ item }">
+                    <v-chip
+                        :color="gradeGetColor(item.rating)"
                     >
-                        <td>{{ item.testNo }}</td>
-                        <td>
-                            <v-chip
-                                :color="autoGradeColor(item.testScore)"
-                            >
-                                {{ item.testScore }}
-                            </v-chip>
-                        </td>
-                        <td>{{ item.processingTime }} </td>
-                        <td>
-                            <v-chip
-                                :color="autoGradeColor(item.testScore)"
-                            >
-                                {{ item.feedback }}
-                            </v-chip>
-                        </td>
-                    </tr>
-                </tbody>
-            </template>
-        </v-simple-table>
-        <v-data-table
-            v-else
-            :headers="gradeHeaders"
-            :items="grades"
-            :expanded.sync="expanded"
-            item-key="name"
-            show-expand
-            class="table-header"
-            disable-sort
-            hide-default-footer
-        >
-            <template v-slot:top class="table-header">
-                <v-toolbar flat>
-                    <v-toolbar-title>Grade</v-toolbar-title>
-                </v-toolbar>
-            </template>
-            <template v-slot:item.rating="{ item }">
-                <v-chip
-                    :color="gradeGetColor(item.rating)"
-                >
-                    {{ item.rating }} {{item.validity}}
-                </v-chip>
-            </template>
-        </v-data-table>
+                        {{ item.rating }} {{item.validity}}
+                    </v-chip>
+                </template>
+                <template v-slot:expanded-item="{headers, item}">
+                    <td :colspan="3">
+                        <p>Submitted files:</p>
+                        <v-simple-table>
+                            <template v-slot default>
+                                <thead>
+                                    <th class="text-left">File</th>
+                                    <th class="text-left">Size</th>
+                                    <th class="text-left">Actions</th>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="file in item.files" :key="file.id">
+                                        <td>{{file.name}}</td>
+                                        <td>{{file.size}}</td>
+                                        <td>
+                                            <v-btn depressed class="docAction" color="primary" @click="downloadSubmissionFile(doc.fileName, doc.id)">
+                                                <v-icon>download</v-icon>
+                                                Download
+                                            </v-btn>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </template>
+                        </v-simple-table>
+                    </td>
+                </template>
+            </v-data-table>
+            <v-alert v-if="overallScore >= minimum"
+                border="left"
+                text
+                colored-border
+                type="success"
+                color="green"
+            > Saatana
+            </v-alert>
+            <v-alert v-else
+                border="left"
+                text
+                colored-border
+                type="error"
+                :color="red"
+            > Overall score: <strong>{{overallScore}}</strong>
+            </v-alert>
+        </div>
     </div>
 </template>
 
@@ -106,9 +170,10 @@
             return{
                 autoGrading: false,
                 autoGradingResults: [],
-                full: 3.0,
-                valid: 2.0,
-                notValid: 0.0,
+                expanded: [],
+                maximum: 3,
+                minimum: 1,
+                overallScore: 0,
                 /*grades: [
                     {
                         feedback: "OK",
@@ -192,10 +257,9 @@
                 api.downloadSubmissionFile(filename, id, this.dbId, this.taskId, this.tabId);
             },
             gradeGetColor(rating) {
-                if(rating === this.full) return 'green'
-                else if(rating >= this.valid) return 'blue'
-                else if (rating < this.valid && rating > this.notValid) return 'yellow'
-                else return 'red'
+                if(rating >= this.maximum) return '#7dcdbe'
+                else if(rating >= this.minimum) return '#ffdca5'
+                else return '#f07387'
             },
             autoGradeColor(score){
                 if(score === 1) return 'green'
