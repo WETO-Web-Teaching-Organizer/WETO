5b005971db926a1776552fd5364bbd9c202b47d5
Paavo Keski-Orvola <paavo.keski-orvola@tuni.fi>
2020-11-29 11:57:26 +0200

    Add minor fixes and formating fixes

--- weto-front/src/components/FileSubmit.vue
+++ weto-front/src/components/FileSubmit.vue
@@ -1,195 +1,224 @@
 <template>
-    <div class="container">
-        <form enctype="multipart/form">
-            <div class="dropbox">
-                <input
-                    name="Browse files"
-                    class="input-file"
-                    multiple
-                    type="file"
-                    ref="files"
-                    @change="selectFile"
-                    @drop="dropFile"
-                />
-                <div class ="dropbox-header">
-                    <p class="call-to-action">Drag and drop file(s) here or click to pick them</p>
-                    <p class="description">{{allowedFilePatterns}}</p>
-                </div>
-                <v-row
-                    align="center"
-                >
-                    <v-col
-                        align-self="auto"
-                        v-for="(file, i) in files"
-                        :key="file.name"
-                    >
-                        <v-chip
-                            close
-                            outlined
-                            color="#32005C"
-                            @click:close="files.splice(i,1)">
-                            <strong>{{file.name}}</strong>
-                        </v-chip>
-                    </v-col>
-                </v-row>
-                <p class="error-message" v-if="this.excluded && this.excludedFiles.length === 1">This file was not accepted: {{this.excludedFiles}}</p>
-                <p class="error-message" v-if="this.excluded && this.excludedFiles.length > 1">These files were not accepted: {{this.excludedFiles}}</p>
-            </div>
-            <v-row justify="center">
-                <v-btn
-                color="#7dcdbe"
-                @click="ensureSubmission"
-                >Add submission file(s)</v-btn>
-                <v-btn
-                @click="removeAllFiles"
-                color="#f07387"
-                >Remove all submission files</v-btn>
-            </v-row>
-        </form>
-    </div>
+  <div class="container">
+    <form enctype="multipart/form">
+      <div class="dropbox">
+        <input
+          name="Browse files"
+          class="input-file"
+          multiple
+          type="file"
+          ref="files"
+          @change="selectFile"
+          @drop="dropFile"
+        />
+        <div class="dropbox-header">
+          <p class="call-to-action">
+            Drag and drop file(s) here or click to pick them
+          </p>
+          <p class="description">{{ allowedFilePatterns }}</p>
+        </div>
+        <v-row align="center">
+          <v-col align-self="auto" v-for="(file, i) in files" :key="file.name">
+            <v-chip
+              close
+              outlined
+              color="#32005C"
+              @click:close="files.splice(i, 1)"
+            >
+              <strong>{{ file.name }}</strong>
+            </v-chip>
+          </v-col>
+        </v-row>
+        <p
+          class="error-message"
+          v-if="this.excluded && this.excludedFiles.length === 1"
+        >
+          This file was not accepted: {{ this.excludedFiles }}
+        </p>
+        <p
+          class="error-message"
+          v-if="this.excluded && this.excludedFiles.length > 1"
+        >
+          These files were not accepted: {{ this.excludedFiles }}
+        </p>
+      </div>
+      <v-row justify="center">
+        <v-btn color="#7dcdbe" @click="ensureSubmission"
+          >Add submission file(s)</v-btn
+        >
+        <v-btn @click="removeAllFiles" color="#f07387"
+          >Remove all submission files</v-btn
+        >
+      </v-row>
+    </form>
+  </div>
 </template>
 <script>
-    import api from '../backend-api'
-    export default {
-        name: 'filesubmit',
-        computed: {
-            status() {
-                return this.$store.getters.status;
-            },
-            taskId() {
-                return this.$store.getters.currentTask;
-            },
-            dbId() {
-                return this.$store.getters.selectedCourse.databaseId;
-            },
-            tabId() {
-                return 4;
-            },
-            taskName() {
-                return this.$store.getters.selectedCourse.name;
-            },
-            user() {
-                return this.$store.getters.user;
-            },
-            subTasks() {
-                return this.$store.getters.subTasks;
-            },
-            allowedFilePatterns() {
-                return (this.filePatterns === "" ? "Allowed file patterns: *.*" : "Allowed file patterns: " + this.filePatterns);
-            }
-        },
-        data(){
-            return {
-                files: [],
-                excludedFiles: [],
-                error: false,
-                excluded: false,
-                description: "",
-            }
-        },
-        props: {
-            submission: Object,
-            filePatterns: String,
-        },
-        methods:{
-            dropFile(event){
-                event.preventDefault();
-                this.$refs.files.files = event.dataTransfer.files;
-                this.selectFile();
-            },
-            selectFile(){
-                const files = this.$refs.files.files;
-                this.files = [...this.files, ...files];
-                this.excluded = false;
-                this.excludedFiles = [];
-            },
-            ensureSubmission() {
-              if (this.submission) {
-                this.uploadFiles();
-              } else {
-                this.$emit('add');
-              }
-            },
-            uploadFiles(){
-                this.excludedFiles = [];
-                this.excluded = false;
-                let i;
-                for(i = 0; i < this.files.length; i++){
-                    this.submitFile(i)
-                }
-            },
-            fileNotAllowed(){
-                this.excluded = true;
-            },
-            submitFile(i){
-                api.addSubmissionFile(this.files[i].name, this.submission.id, this.dbId, this.taskId, this.tabId).then(response => {
-                    if(response.data.excludedFiles.length > 0){
-                        this.excludedFiles = [...this.excludedFiles, ...response.data.excludedFiles];
-                        this.fileNotAllowed();
-                    }
-                }).then(() => {
-                    this.sendSubmission(i);
-                }).catch(err => {
-                    console.log(err);
-                });
-            },
-            sendSubmission(i){
-                api.fileSubmission(this.files[i], this.submission.id, this.dbId, this.taskId, this.tabId).then(() => {
-                  this.$emit('refresh');
-                }).catch(err => {
-                    console.log(err);
-                })
-            },
-            removeAllFiles(){
-                this.files = [];
-                this.excluded = false;
-                this.excludedFiles = [];
-            }
-        }
-    }
+import api from "../backend-api";
+export default {
+  name: "filesubmit",
+  computed: {
+    status() {
+      return this.$store.getters.status;
+    },
+    taskId() {
+      return this.$store.getters.currentTask;
+    },
+    dbId() {
+      return this.$store.getters.selectedCourse.databaseId;
+    },
+    tabId() {
+      return 4;
+    },
+    taskName() {
+      return this.$store.getters.selectedCourse.name;
+    },
+    user() {
+      return this.$store.getters.user;
+    },
+    subTasks() {
+      return this.$store.getters.subTasks;
+    },
+    allowedFilePatterns() {
+      return this.filePatterns === ""
+        ? "Allowed file patterns: *.*"
+        : "Allowed file patterns: " + this.filePatterns;
+    },
+  },
+  data() {
+    return {
+      files: [],
+      excludedFiles: [],
+      error: false,
+      excluded: false,
+      description: "",
+    };
+  },
+  props: {
+    submission: Object,
+    filePatterns: String,
+  },
+  methods: {
+    dropFile(event) {
+      event.preventDefault();
+      this.$refs.files.files = event.dataTransfer.files;
+      this.selectFile();
+    },
+    selectFile() {
+      const files = this.$refs.files.files;
+      this.files = [...this.files, ...files];
+      this.excluded = false;
+      this.excludedFiles = [];
+    },
+    ensureSubmission() {
+      if (this.submission) {
+        this.uploadFiles();
+      } else {
+        this.$emit("add");
+      }
+    },
+    uploadFiles() {
+      this.excludedFiles = [];
+      this.excluded = false;
+      let i;
+      for (i = 0; i < this.files.length; i++) {
+        this.submitFile(i);
+      }
+    },
+    fileNotAllowed() {
+      this.excluded = true;
+    },
+    submitFile(i) {
+      api
+        .addSubmissionFile(
+          this.files[i].name,
+          this.submission.id,
+          this.dbId,
+          this.taskId,
+          this.tabId
+        )
+        .then((response) => {
+          if (response.data.excludedFiles.length > 0) {
+            this.excludedFiles = [
+              ...this.excludedFiles,
+              ...response.data.excludedFiles,
+            ];
+            this.fileNotAllowed();
+          }
+        })
+        .then(() => {
+          this.sendSubmission(i);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    sendSubmission(i) {
+      api
+        .fileSubmission(
+          this.files[i],
+          this.submission.id,
+          this.dbId,
+          this.taskId,
+          this.tabId
+        )
+        .then(() => {
+          this.$emit("refresh");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    removeAllFiles() {
+      this.files = [];
+      this.excluded = false;
+      this.excludedFiles = [];
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .container {
-        margin-bottom: 20px;
-    }
-    .dropbox {
-        min-height: 200px;
-        padding: 10px 10px;
-        position: relative;
-        cursor: pointer;
-        box-shadow: 0 0 10px lightgrey;
-        background: white;
-        border: 10px solid #F3E5FF;
-        margin-bottom: 10px
-    }
-    .input-file {
-        opacity: 0;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-    }
-    .dropbox:hover {
-        background: #E0E0E0;
-    }
-    .dropbox .call-to-action{
-        color: #32005C;
-        font-size: 1.5rem;
-        text-align: center;
-    }
-    .dropbox .error-message{
-        color: red;
-        font-size: 1rem;
-        text-align: center;
-        border: 5px solid red;
-        padding: 10px 0;
-    }
-    .dropbox-header{
-        padding: 75px 0;
-    }
-    .description {
-        color: #32005C99;
-        font-size: 1.2rem;
-        text-align: center;
-    }
+.container {
+  margin-bottom: 20px;
+}
+.dropbox {
+  min-height: 200px;
+  padding: 10px 10px;
+  position: relative;
+  cursor: pointer;
+  box-shadow: 0 0 10px lightgrey;
+  background: white;
+  border: 10px solid #f3e5ff;
+  margin-bottom: 10px;
+}
+.input-file {
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+.dropbox:hover {
+  background: #e0e0e0;
+}
+.dropbox .call-to-action {
+  color: #32005c;
+  font-size: 1.5rem;
+  text-align: center;
+}
+.dropbox .error-message {
+  color: red;
+  font-size: 1rem;
+  text-align: center;
+  border: 5px solid red;
+  padding: 10px 0;
+}
+.dropbox-header {
+  padding: 45px 0px 5px 0px;
+}
+.description {
+  color: #32005c99;
+  font-size: 1.2rem;
+  text-align: center;
+}
 </style>
--- weto-front/src/components/UserSubmission.vue
+++ weto-front/src/components/UserSubmission.vue
@@ -12,22 +12,30 @@
           </tr>
         </thead>
         <tbody>
-          <tr
-            v-for="doc in documents" :key="doc.id"
-          >
+          <tr v-for="doc in documents" :key="doc.id">
             <td>{{ doc.fileName }}</td>
             <td>{{ doc.fileDate }}</td>
             <td>{{ doc.fileSize }} bytes</td>
             <td>{{ getSubmissionStatus }}</td>
             <td>
               <v-row>
-                <v-btn depressed class="docAction" color="primary" @click="downloadSubmissionFile(doc.fileName, doc.id)">
+                <v-btn
+                  depressed
+                  class="docAction"
+                  color="primary"
+                  @click="downloadSubmissionFile(doc.fileName, doc.id)"
+                >
                   <v-icon>download</v-icon>
                   Download
                 </v-btn>
                 <!-- <v-btn depressed color="secondary" class="docAction">Edit</v-btn> -->
                 <template>
-                  <v-btn depressed class="docAction" color="error" @click="showDeleteDialog(doc)">
+                  <v-btn
+                    depressed
+                    class="docAction"
+                    color="error"
+                    @click="showDeleteDialog(doc)"
+                  >
                     <v-icon>delete</v-icon>
                     Delete
                   </v-btn>
@@ -49,12 +57,21 @@
           Delete file from submission
         </v-card-title>
         <v-card-text>
-          Are you sure you want to delete the file: {{documentToDelete.fileName}}?
+          Are you sure you want to delete the file:
+          {{ documentToDelete.fileName }}?
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="secondary" text @click="dialog = false">Cancel</v-btn>
-          <v-btn color="primary" text @click="dialog = false; deleteSubmissionFile(documentToDelete.id);">Delete</v-btn>
+          <v-btn
+            color="primary"
+            text
+            @click="
+              dialog = false;
+              deleteSubmissionFile(documentToDelete.id);
+            "
+            >Delete</v-btn
+          >
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -62,76 +79,87 @@
 </template>
 
 <script>
-  import api from '../backend-api'
-  export default {
-    name: 'userSubmission',
-    data() {
-      return {
-        backendResponse: [],
-        errors: [],
-        dialog: false,
-        documentToDelete: {}
-      }
+import api from "../backend-api";
+export default {
+  name: "userSubmission",
+  data() {
+    return {
+      backendResponse: [],
+      errors: [],
+      dialog: false,
+      documentToDelete: {},
+    };
+  },
+  props: {
+    submissionStatus: String,
+    submission: Object,
+    documents: Array,
+    submissionPeriodActive: Boolean,
+  },
+  computed: {
+    taskId() {
+      return this.$store.getters.currentTask;
     },
-    props: {
-      submissionStatus: String,
-      submission: Object,
-      documents: Array,
-      submissionPeriodActive: Boolean,
+    dbId() {
+      return this.$store.getters.selectedCourse.databaseId;
     },
-    computed: {
-      taskId() {
-        return this.$store.getters.currentTask;
-      },
-      dbId() {
-        return this.$store.getters.selectedCourse.databaseId;
-      },
-      tabId() {
-        return 4;
-      },
-      taskName() {
-        return this.$store.getters.selectedCourse.name;
-      },
-      getSubmissionStatus() {
-        return (this.submissionStatus === 'Accepted' ? 'Submitted' : this.submissionStatus)
-      }
+    tabId() {
+      return 4;
     },
-    methods: {
-      downloadSubmissionFile(filename, id) {
-        api.downloadSubmissionFile(filename, id, this.dbId, this.taskId, this.tabId);
-      },
-      deleteSubmissionFile(id) {
-        api.deleteSubmissionFile(id, this.dbId, this.taskId, this.tabId).then(() => {
-          this.$emit('deleteDocument');
-        }).catch(err => {
+    taskName() {
+      return this.$store.getters.selectedCourse.name;
+    },
+    getSubmissionStatus() {
+      return this.submissionStatus === "Accepted"
+        ? "Submitted"
+        : this.submissionStatus;
+    },
+  },
+  methods: {
+    downloadSubmissionFile(filename, id) {
+      api.downloadSubmissionFile(
+        filename,
+        id,
+        this.dbId,
+        this.taskId,
+        this.tabId
+      );
+    },
+    deleteSubmissionFile(id) {
+      api
+        .deleteSubmissionFile(id, this.dbId, this.taskId, this.tabId)
+        .then(() => {
+          this.$emit("deleteDocument");
+        })
+        .catch((err) => {
           console.log(err);
           this.backendResponse.push(err);
         });
-      },
-      showDeleteDialog(document) {
-        this.documentToDelete = document;
-        this.dialog = !this.dialog;
-      },
-    }
-  }
+    },
+    showDeleteDialog(document) {
+      this.documentToDelete = document;
+      this.dialog = !this.dialog;
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .row {
-    align-content: center;
-    justify-content: flex-start;
-  }
-  .v-data-table {
-    margin: 1em;
-    border: 1px #102027 solid;
-  }
-  thead {
-    background-color: #f3e5ff;
-  }
-  .docAction {
-    margin-right: 2em;
-    margin-top: 0.5em;
-    margin-bottom: 0.5em;
-    padding-left: 12px;
-  }
+.row {
+  align-content: center;
+  justify-content: flex-start;
+}
+.v-data-table {
+  margin: 1em;
+  border: 1px #102027 solid;
+}
+thead {
+  background-color: #f3e5ff;
+}
+.docAction {
+  margin-right: 2em;
+  margin-top: 0.5em;
+  margin-bottom: 0.5em;
+  padding-left: 12px;
+}
 </style>
